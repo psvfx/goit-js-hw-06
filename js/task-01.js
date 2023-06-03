932a88dbfa06d3f8a809de9b0b4614a873391866
psvfx <psvfxinvest@gmail.com>
2023-06-03 19:30:08 +0300

    add base and task 1

---
+++ js/task-01.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const categoriesEl = document.querySelector("#categories");
+const numberOfCategories = `Number of categories: ${categoriesEl.children.length}`;
+console.log(numberOfCategories);
+
+[...categoriesEl.children].forEach((item) => {
+  const textContentItem = `Category: ${item.firstElementChild.textContent}`;
+  console.log(textContentItem);
+
+  const numberOfElements = `Elements: ${item.lastElementChild.length}`;
+  console.log(numberOfElements);
+});
