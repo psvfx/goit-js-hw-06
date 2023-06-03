9bf11a99e50547d4f770f16cb47798dd6a536f0a
psvfx <psvfxinvest@gmail.com>
2023-06-03 20:40:06 +0300

    Update task-1

--- js/task-01.js
+++ js/task-01.js
@@ -1,13 +1,17 @@
 "use strict";
 
 const categoriesEl = document.querySelector("#categories");
+
 const numberOfCategories = `Number of categories: ${categoriesEl.children.length}`;
-console.log(numberOfCategories);
+
+// console.log(numberOfCategories);
 
 [...categoriesEl.children].forEach((item) => {
   const textContentItem = `Category: ${item.firstElementChild.textContent}`;
-  console.log(textContentItem);
 
-  const numberOfElements = `Elements: ${item.lastElementChild.length}`;
-  console.log(numberOfElements);
+  //   console.log(textContentItem);
+
+  const numberOfElements = `Elements: ${item.lastElementChild.children.length}`;
+
+  //   console.log(numberOfElements);
 });
