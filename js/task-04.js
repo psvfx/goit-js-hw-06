4aead8a4395463beb4088ceee86911e410d73c01
psvfx <psvfxinvest@gmail.com>
2023-06-04 00:55:39 +0300

    solution of task 4

---
+++ js/task-04.js
@@ -0,0 +1,16 @@
+"use strict";
+
+let counterValue = 0;
+const buttonDecrement = document.querySelector(`[data-action = "decrement"]`);
+const buttonIncrement = document.querySelector(`[data-action = "increment"]`);
+const value = document.querySelector("#value");
+
+buttonDecrement.addEventListener("click", () => {
+  counterValue -= 1;
+  value.textContent = counterValue;
+});
+
+buttonIncrement.addEventListener("click", () => {
+  counterValue += 1;
+  value.textContent = counterValue;
+});
