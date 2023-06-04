0e8187209dd3e36323efc40c6fd80d4d41ff6f1f
psvfx <psvfxinvest@gmail.com>
2023-06-04 16:28:25 +0300

    solution of task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,15 @@
+"use strict";
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const divEl = document.querySelector(".widget");
+
+divEl.lastElementChild.addEventListener("click", () => {
+  divEl.parentNode.style.backgroundColor = getRandomHexColor();
+
+  divEl.firstElementChild.firstElementChild.textContent = getRandomHexColor();
+});
