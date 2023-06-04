9034f2ed42873c577a02c737f078dd8dcb8d3fc6
psvfx <psvfxinvest@gmail.com>
2023-06-04 11:54:23 +0300

    solution of task 6

---
+++ js/task-06.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const inputEl = document.getElementById("validation-input");
+
+inputEl.addEventListener("blur", () => {
+  if (inputEl.value.length == inputEl.dataset.length) {
+    inputEl.classList.add("valid");
+    inputEl.classList.remove("invalid");
+  } else {
+    inputEl.classList.add("invalid");
+    inputEl.classList.remove("valid");
+  }
+});
