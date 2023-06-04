9179a26359767ee3f6e594d8b0ce0a1a647b08d2
psvfx <psvfxinvest@gmail.com>
2023-06-04 11:14:26 +0300

    solution of task 5

---
+++ js/task-05.js
@@ -0,0 +1,14 @@
+"use strict";
+
+const inputEl = document.querySelector("#name-input");
+console.log(inputEl);
+
+const outputEl = document.querySelector("#name-output");
+
+inputEl.addEventListener("input", (el) => {
+  outputEl.textContent = el.currentTarget.value;
+
+  if (el.currentTarget.value === "") {
+    outputEl.textContent = "Anonymous";
+  }
+});
